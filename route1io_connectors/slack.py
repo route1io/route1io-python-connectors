4332e355816b578a0cde0f5b463c527a9af5a9de
chris-greening <chrisgreening3@gmail.com>
2021-07-20 16:36:50 -0400

    FIX: added webhook parameter

--- route1io_connectors/slack.py
+++ route1io_connectors/slack.py
@@ -8,33 +8,37 @@ from typing import Dict
 
 import requests
 
-def slack_message(message: str) -> None:
+def slack_message(message: str, webhook: str) -> None:
     """Send plain message to Slack
 
     Parameters
     ----------
     message : str
         Single string message to be sent to Slack
+    webhook : str
+        WebHook for sending the message to
     """
     message_json = json.dumps({"text": message})
     _post_slack(data=message_json)
 
-def slack_block_message(message: Dict[str, str]) -> None:
+def slack_block_message(message: Dict[str, str], webhook: str) -> None:
     """Send formatted block message to Slack
 
     Parameters
     ----------
     message : Dict[str, str]
         Dictionary containing the formatted block message to be sent to Slack
+    webhook : str
+        WebHook for sending the message to
     """
     message_json = json.dumps(message)
-    _post_slack(data=message_json)
+    _post_slack(data=message_json, webhook=webhook)
 
-def _post_slack(data: str) -> None:
+def _post_slack(data: str, webhook: str) -> None:
     """Send POST request to Slack Webhook for message"""
     headers = {"Content-type": "application/json"}
     resp = requests.post(
-        env.SLACK_WEBHOOK,
+        webhook,
         headers=headers,
         data=data
     )
