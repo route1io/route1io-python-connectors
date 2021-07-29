161f4a7c4ba2f7669426dc6a65d644b613d6e8a4
chris-greening <chrisgreening3@gmail.com>
2021-07-29 16:12:55 -0400

    ADD: added version bump param check and bump2version

--- bin/pypi.bash
+++ bin/pypi.bash
@@ -1,4 +1,16 @@
+# Author: Chris Greening
+# Date: 07/29/2021
+# Purpose: Upload the package to PyPI
+
 # Push release to PyPI
+
+if [[ $1 == "" ]]
+then
+    echo "Please provide a Semantic version bump parameter i.e. major, minor, or patch"
+fi
+
+# Increse Semantic version number
+bump2version $1
 
 # Create dist 
 python3 setup.py sdist bdist_wheel
