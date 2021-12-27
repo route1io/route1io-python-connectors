467024cd74cbed3fdaaaffa7fb97e35f07425d30
Chris <chris@christophergreening.com>
2021-12-27 11:57:20 -0500

    FIX: set packages equal to find_packages() to dynamically find subpackages

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ README = HERE.joinpath("README.md").read_text()
 
 setup(
     name="route1io-connectors",
-    version="0.2.1",
+    version="0.3.1",
     description="Connectors for interacting with popular API's used in marketing analytics using clean and concise Python code.",
     long_description=README,
     long_description_content_type="text/markdown",
@@ -18,7 +18,7 @@ setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Programming Language :: Python :: 3",
     ],
-    packages=["route1io_connectors"],
+    packages=find_packages(),
     include_package_data=True,
     install_requires=["boto3", "google-api-python-client", 
                       "google-auth-httplib2", "google-auth-oauthlib",
