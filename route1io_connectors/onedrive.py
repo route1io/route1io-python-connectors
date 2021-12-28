884e2f7833e91fcf98b7a377d2ef36ac024744a3
Chris <chris@christophergreening.com>
2021-12-27 22:42:09 -0500

    REFACTOR: refactored request_access_token to use private functions

---
+++ route1io_connectors/onedrive.py
@@ -0,0 +1,83 @@
+"""OneDrive
+
+The purpose of this module is for uploading/downloading files to/from OneDrive and SharePoint
+"""
+
+import webbrowser
+from typing import List
+import json
+
+import requests 
+import jwt
+
+def permissions_prompt(tenant_id: str, client_id: str, scope: List[str]) -> str:
+    """Convenience function for opening web browser to permissions prompt"""
+    url = f"https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/authorize?client_id={client_id}&response_type=code&scope={'%20'.join(scope)}&prompt=consent"
+    webbrowser.open(url)
+
+def request_access_token(
+        code: str, 
+        tenant_id: str, 
+        client_id: str, 
+        scope: List[str], 
+        redirect_uri: str, 
+        client_secret: str
+    ) -> dict:
+    """Return a dict of authentication information"""
+    resp = requests.post(
+        data = _encode_payload(
+            client_id=client_id,
+            scope=_encode_scope(scope),
+            code=code,
+            redirect_uri=redirect_uri,
+            grant_type="authorization_code",
+            client_secret=client_secret
+        ),        
+        url = _get_token_url(tenant_id=tenant_id)
+    )
+    resp = json.loads(resp)
+    return resp
+
+def refresh_access_token(
+        client_id: str, 
+        client_secret: str, 
+        refresh_token: str, 
+        scope: List[str], 
+        tenant_id: str
+    ):
+    resp = requests.post(
+        data=_encode_payload(
+            client_id=client_id,
+            client_secret=client_secret,
+            refresh_token=refresh_token,
+            grant_type="refresh_token",
+            scope=_encode_scope(scope)
+        ),
+        url=_get_token_url(tenant_id=tenant_id)
+    )
+    resp = json.loads(resp.text)
+    return resp 
+
+def _get_token_url(tenant_id: str) -> str:
+    """Return token URL built form the given tenant ID"""
+    return f"https://login.microsoftonline.com/{tenant_id}/oauth2/v2.0/token"
+
+def _encode_payload(**kwargs) -> str:
+    """Return dictionary payload as a string formatted for POST request"""
+    return "&".join(
+        f"{key}={val}" for key, val in kwargs.items()
+    )
+
+def _encode_scope(scope: List[str]) -> str:
+    """Return scope encoded as a string for a URL query param"""
+    return '%20'.join(scope)
+
+if __name__ == "__main__":
+    import dotenv
+    env = dotenv.dotenv_values()
+    data = refresh_access_token(
+    tenant_id=env['tenant_id'],
+    client_id=env['client_id'],
+    refresh_token=env['refresh_token'],
+    scope=["user.read", "Sites.Read.All", "offline_access"],
+    client_secret=env['client_secret'])
