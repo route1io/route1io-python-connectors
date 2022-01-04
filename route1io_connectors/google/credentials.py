3014820f9ed827379f7fe3a60f70c30d82d51f0c
chris-greening <chrisgreening3@gmail.com>
2022-01-04 17:13:09 -0500

    REMOVE: removed outdated versions of credential refresh

--- route1io_connectors/google/credentials.py
+++ route1io_connectors/google/credentials.py
@@ -79,39 +79,3 @@ def refresh_token_from_credentials(refresh_token: str,
     )
     creds.refresh(Request())
     return creds
-
-def get_google_credentials(refresh_token: str, cid: str, csc: str) -> "google.oath2.credentials.Credentials":
-
-    """Return a Credentials object containing refreshed access token
-
-    Parameters
-    ----------
-    refresh_token : str
-        Valid refresh token for getting a new access token
-    cid : str
-        Client ID from GCP console
-    csc : str
-        Client secret from GCP console
-
-    Returns
-    -------
-    creds : google.oath2.credentials.Credentials
-        Valid access credentials for accessing Google API
-    """
-    data = {
-        "refresh_token": refresh_token,
-        "client_id": cid,
-        "client_secret": csc,
-        "grant_type": "refresh_token"
-    }
-    resp = requests.post(endpoints.GOOGLE_TOKEN_ENDPOINT, data=data)
-    access_token_data = json.loads(resp.text)
-    access_token = access_token_data["access_token"]
-    creds = Credentials(token=access_token)
-    return creds
-
-def get_refresh_token(credentials_fpath: str, endpoints: List[str], port: int = 8080) -> str:
-    """Opens Google auth screen and returns a refresh token"""
-    flow = InstalledAppFlow.from_client_secrets_file(credentials_fpath, endpoints)
-    creds = flow.run_local_server(port=port)
-    return creds.refresh_token
