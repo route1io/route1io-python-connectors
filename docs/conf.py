654c95e8cdcb962d24782f7b59f13dfb4154a725
chris-greening <chrisgreening3@gmail.com>
2022-03-20 14:12:29 -0400

    DOCS: added add_module_names = False

--- docs/conf.py
+++ docs/conf.py
@@ -45,6 +45,8 @@ extensions = [
     'sphinx.ext.intersphinx'
 ]
 autosummary_generate = True
+add_module_names = False
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
