55e05c67892a7712f2d002e9266588356c7ad213
Chris <chris@christophergreening.com>
2022-07-11 15:29:19 -0400

    REFACTOR: removed unnecessary whitespace

--- route1io_connectors/onedrive.py
+++ route1io_connectors/onedrive.py
@@ -90,7 +90,6 @@ def upload_file(access_token: str, url: str, fpath: str, chunk_size: int = DEFAU
             )
     return json.loads(resp.text)
 
-
 def copy_file_to_aws_s3(access_token: str, url: str, s3, bucket: str, key: str = None) -> None:
     """Copy file at given URL to S3 bucket
 
