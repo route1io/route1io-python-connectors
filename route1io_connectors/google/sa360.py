ee0876181f09287383590a112234b1bc6980479c
Christopher Greening <chris@christophergreening.com>
2024-06-24 23:30:20 -0400

    ADD: added _get_post_request_payload func def

--- route1io_connectors/google/sa360.py
+++ route1io_connectors/google/sa360.py
@@ -3,7 +3,7 @@
 This module contains code for accessing data from Search Ads 360.
 """
 
-from typing import Optional, Dict
+from typing import Optional, Dict, Union
 
 import pandas as pd
 
@@ -14,12 +14,17 @@ def get_sa360_data(access_token: str, account_id: str, query: str, login_custome
     report_url = _get_report_url(account_id)
     headers = _get_post_request_header(access_token, login_customer_id)
 
+def _get_post_request_payload(query: str) -> Dict[str, Union[bool, str]]:
+    """Return dictionary of POST request payload data"""
+    pass
+
 def _get_post_request_header(access_token: str, login_customer_id: str) -> Dict[str, str]:
-    """Return header for POST request"""
+    """Return dictionary of POST request header data"""
     headers={"Authorization": f"Bearer {access_token}"}
     if login_customer_id is not None:
         headers["login_customer_id"] = login_customer_id
     return headers
 
 def _get_report_url(account_id: str) -> str:
+    """Return string to report URL for a given account_id"""
     return f"https://searchads360.googleapis.com/v0/customers/{account_id}/searchAds360:search"
