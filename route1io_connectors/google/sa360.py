dfb055fbfa0f9a3eaa554a05052d74c7930cfb2c
Christopher Greening <chris@christophergreening.com>
2024-06-24 23:20:51 -0400

    ADD: added func def for get_sa360_data

--- route1io_connectors/google/sa360.py
+++ route1io_connectors/google/sa360.py
@@ -2,3 +2,13 @@
 
 This module contains code for accessing data from Search Ads 360.
 """
+
+from typing import Optional
+
+import pandas as pd
+
+def get_sa360_data(access_token: str, account_id: str, query: str, login_customer_id: Optional[str] = None) -> "pd.DataFrame":
+    """Return SA360 data requested from the SA360 Reporting API and processed
+    into a pd.DataFrame
+    """
+    pass
